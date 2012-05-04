7f344a6b94c90d6366d113f0ce85bce011c3f6cf
Åke Argéus <ake@mat.se>
2012-05-04 10:19:07 +0200

    Snapshot

--- MaglevGrailsPlugin.groovy
+++ MaglevGrailsPlugin.groovy
@@ -9,7 +9,7 @@ import org.codehaus.groovy.grails.web.context.ServletContextHolder
 
 class MaglevGrailsPlugin {
     // the plugin version
-    def version = "0.3.4"
+    def version = "0.3.5-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.0 > *"
     // the other plugins this plugin depends on
